0ba126336d88157fc65662afb36387c2d2b1b7c5
sajib <ashiqur.rahman@technuf.com>
2026-03-14 00:56:00 +0600

    Fix: Finalize gender field in RegisterDto

--- qabool_backend/src/auth/dto/register.dto.ts
+++ qabool_backend/src/auth/dto/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, MaxLength, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
